6def0f0b5d002e6743002f1b64266b07edf32056
ryraansh <ryraansh@gmail.com>
2017-11-29 15:27:07 -0700

    Finished XboxMap

--- src/robot/xbox/XboxMap.java
+++ src/robot/xbox/XboxMap.java
@@ -2,4 +2,24 @@ package robot.xbox;
 
 public class XboxMap {
 	
+	//Sticks and Triggers
+	public static final int LeftJoystickX = 0;
+	public static final int LeftJoystickY = 1;
+	public static final int LeftTrigger = 2;
+	public static final int RightTrigger = 3;
+	public static final int RightJoystickX = 4;
+	public static final int RightJoyStickY = 5;
+	
+	//Buttons
+	public static final int AButton = 1;
+	public static final int Button = 2;
+	public static final int XButton = 3;
+	public static final int YButton = 4;
+	public static final int LeftBumper = 5;
+	public static final int RightBumper = 6;
+	public static final int BackButton = 7;
+	public static final int StartButton = 8;
+	public static final int LeftStickButton = 9;
+	public static final int RightStickButton = 10;
+	
 }
