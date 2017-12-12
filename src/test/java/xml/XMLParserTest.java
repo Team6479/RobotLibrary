c2acdf621d51a29ef435ca3af53e8c6c91987adc
jacob-abraham <jacob.r.abraham@gmail.com>
2017-12-11 18:41:19 -0700

    made test better

--- src/test/java/xml/XMLParserTest.java
+++ src/test/java/xml/XMLParserTest.java
@@ -2,9 +2,11 @@ package xml;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -15,12 +17,29 @@ import robot.config.tree.Robot;
 //test the parsing of the xml file
 public class XMLParserTest {
 
+	private File xml;
+	private String parsedStr;
+	
+	@Before
+	public void setupTest() throws IOException {
+		//get xml file
+		xml = new File(getClass().getClassLoader().getResource("systems.xml").getFile());
+		
+		//get parsed file
+		File parsed = new File(getClass().getClassLoader().getResource("ParsedXML").getFile());
+		parsedStr = "";
+		BufferedReader read = new BufferedReader(new FileReader(parsed));
+		int next = 0;
+		while((next = read.read()) != -1) {
+			parsedStr += (char)next;
+		}
+		read.close();
+	}
+	
 	@Test
 	public void shouldParseXMLIntoValidRobotConfig() {
 		try 
 		{
-			//get xml file
-			File xml = new File(getClass().getClassLoader().getResource("systems.xml").getFile());
 			//parse the xml
 			Robot robot = ConfigParser.parseXML(xml);
 			String robotStr = robot.toString();
@@ -28,20 +47,7 @@ public class XMLParserTest {
 			//if returns no Robot, failure
 			assertNotNull("Robot config cannot be null", robot);
 
-			//get parsed file
-			File parsed = new File(getClass().getClassLoader().getResource("ParsedXML").getFile());
-			String parsedStr = "";
-			BufferedReader read = new BufferedReader(new FileReader(parsed));
-			int next = 0;
-			while((next = read.read()) != -1) {
-				parsedStr += (char)next;
-			}
-			read.close();
-
 			assertEquals("", parsedStr, robotStr);
-		}
-		catch (IOException e) {
-			fail("Error reading file");
 		}
 		catch (NullPointerException e) {
 			fail("Error parsing xml");
