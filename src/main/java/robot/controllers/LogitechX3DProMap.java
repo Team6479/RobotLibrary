f57c6d64f36250aaefe09e75f8b956a2d7e0e9b9
Osnott <aidenonstott@gmail.com>
2018-11-26 16:47:01 -0700

    Added Logitech X 3D Pro Map

---
+++ src/main/java/robot/controllers/LogitechX3DProMap.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package robot.controllers;
+
+/**
+ * The LogitechX3DProMap holds all of the ports for the buttons,
+ * triggers, and stick. These integers can be accesed
+ * through the OI to make a joystick object.
+ * Use this class to remove magic numbers.
+ *
+ * @author Aiden Onstott
+ */
+public class LogitechX3DProMap {
+    // Joystick Controllers
+    public static final int joystick = 0;
+
+    // Joystick Buttons
+    public static final int joystickButton1 = 1;
+    public static final int joystickButton2 = 2;
+    public static final int joystickButton3 = 3;
+    public static final int joystickButton4 = 4;
+    public static final int joystickButton5 = 5;
+    public static final int joystickButton6 = 6;
+    public static final int joystickButton7 = 7;
+    public static final int joystickButton8 = 8;
+    public static final int joystickButton9 = 9;
+    public static final int joystickButton10 = 10;
+    public static final int joystickButton11 = 11;
+    public static final int joystickButton12 = 12;
+    public static final int joystickButton13 = 13;
+}
